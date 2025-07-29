315347d0c5ca927a24b9f165c97c5b17911eec8b
FreddyJ01 <fahrodej01@gmail.com>
2025-07-29 11:19:09 -0500

    Added Game Board

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2274f6a41f9353402dc755c7b52c7e5a297e1716")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b327cddfef3121e6169168e53fdb635c122c014")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
