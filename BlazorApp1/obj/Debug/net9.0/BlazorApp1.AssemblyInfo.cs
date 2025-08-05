31309277fb2f697b520e80bb6326f44d0c4e81c0
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-05 12:03:16 -0400

    update battleship

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96caca8541334a8682e08f703d305fbe07628c78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c152a4e3cfb22ce113da9c9981556c73a8cf4963")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
